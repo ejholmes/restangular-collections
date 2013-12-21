b06f657e8f1f2e81d62f912b0416f2e997b57432
Eric J. Holmes <eric@ejholmes.net>
2013-12-21 11:23:54 -0800

    Build.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,10 +4,37 @@ module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
 
   grunt.initConfig({
+    pkg: require('./bower.json'),
     karma: {
       unit: {
         configFile: 'karma.conf.js',
         singleRun: process.env.CI
+      }
+    },
+    ngmin: {
+      dist: {
+        src: ['src/restangular-collections.js'],
+        dest: '.tmp/restangular-collections.js'
+      }
+    },
+    concat: {
+      options: {
+        banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+          '<%= grunt.template.today("yyyy-mm-dd") %> */'
+      },
+      dist: {
+        src: ['.tmp/restangular-collections.js'],
+        dest: 'dist/restangular-collections.js'
+      }
+    },
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      dist: {
+        files: {
+          'dist/restangular-collections.min.js': 'dist/restangular-collections.js'
+        }
       }
     },
     jshint: {
@@ -16,5 +43,6 @@ module.exports = function (grunt) {
   });
 
   grunt.registerTask('test', ['jshint', 'karma:unit'])
+  grunt.registerTask('build', ['ngmin', 'concat', 'uglify']);
   grunt.registerTask('default', ['test']);
 };
---
+++ dist/restangular-collections.js
@@ -0,0 +1,106 @@
+/*! restangular-collections - v0.0.1 - 2013-12-21 */(function (angular) {
+  'use strict';
+  var module = angular.module('restangularCollections', ['restangular']);
+  function Collection(elem, options) {
+    var defaults = {
+        id: 'id',
+        methods: { create: 'create' }
+      };
+    this.options = _.extend(defaults, options);
+    this.restangularElem = elem;
+    this.models = [];
+    this.reset();
+  }
+  Collection.prototype.create = function (attributes) {
+    var create = this.restangularElem[this.options.methods.create];
+    return create(attributes).then(_.bind(this.add, this));
+  };
+  Collection.prototype.find = function (model) {
+    var id = this.options.id, find;
+    if (_.isNumber(model)) {
+      find = function (existing) {
+        return existing[id] === model;
+      };
+    } else {
+      find = function (existing) {
+        return existing === model;
+      };
+    }
+    return _.find(this.models, find) || model[id] && this.find(model[id]);
+  };
+  Collection.prototype.add = function (model) {
+    var existing = this.find(model);
+    if (existing) {
+      if (existing != model) {
+        this.replace(existing, model);
+      }
+    } else {
+      this.models.push(model);
+      this.length++;
+      this._addReference(model);
+    }
+    return model;
+  };
+  Collection.prototype.replace = function (existing, model) {
+    angular.copy(model, existing);
+  };
+  Collection.prototype.addAll = function (models) {
+    return angular.forEach(models, _.bind(this.add, this));
+  };
+  Collection.prototype.remove = function (model) {
+    var existing = this.find(model);
+    if (existing) {
+      this.models.splice(_.indexOf(this.models, model), 1);
+      this._removeReference(existing);
+      this.length--;
+    }
+    return model;
+  };
+  Collection.prototype.removeAll = function (models) {
+    return angular.forEach(models, _.bind(this.remove, this));
+  };
+  Collection.prototype.reset = function () {
+    this.models.length = 0;
+    this.length = 0;
+  };
+  Collection.prototype._addReference = function (model) {
+    var collection = this;
+    if (!model.collection) {
+      model.collection = collection;
+      model.destroy = function () {
+        return model.remove().then(_.bind(collection.remove, collection));
+      };
+    }
+  };
+  Collection.prototype._removeReference = function (model) {
+    if (this === model.collection) {
+      delete model.collection;
+    }
+  };
+  Collection.prototype.getList = function () {
+    var promise = this.restangularElem.getList.apply(this.restangularElem, arguments);
+    return promise.then(_.bind(this.addAll, this));
+  };
+  module.config([
+    'RestangularProvider',
+    function (RestangularProvider) {
+      RestangularProvider.setOnElemRestangularized(function (elem, isCollection, what, Restangular) {
+        if (isCollection) {
+          elem.getCollection = function () {
+            return new Collection(elem);
+          };
+        }
+        return elem;
+      });
+    }
+  ]);
+  module.provider('RestangularCollection', function () {
+    var collectionClass = Collection;
+    this.setCollectionClass = function (klass) {
+      collectionClass = klass;
+    };
+    this.$get = function () {
+      return collectionClass;
+    };
+  });
+}(angular));
